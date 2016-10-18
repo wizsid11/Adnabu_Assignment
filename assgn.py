95224dc6aca59fc3ec1bea2963804a4bc2eaa515
wizsid11 <siddharth.mahapatra95@gmail.com>
2016-10-19 01:52:48 +0530

    change in solution

--- assgn.py
+++ assgn.py
@@ -4,8 +4,8 @@ b = ["hello world python","python world","python"]
 c=["scala world","python closure world scala"]
 root_list=[]
 ans=dict((s,[1,s.split(" "),len(s.split(" "))]) for s in a)# creating the ans dict
-all_lengths = np.array(list(set(map(lambda x:x[2],ans.values())))).tolist()
-min_length = np.array(list(set(map(lambda x:x[2],ans.values())))).min()# calculating the minimum length
+all_lengths = list(set(map(lambda x:x[2],ans.values())))
+min_length = np.array(all_lengths).min()# calculating the minimum length
 for key,value in ans.items():
 	if value[2]==min_length:
 		root_list.append(key)
