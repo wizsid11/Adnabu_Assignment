b269bfebf3d26ac2bc3d3974dba1ed1529c826fe
wizsid11 <siddharth.mahapatra95@gmail.com>
2016-10-19 02:51:53 +0530

    change in solution

--- assgn.py
+++ assgn.py
@@ -1,6 +1,6 @@
 import numpy as np
 a=["hello world python","scala world","java python scala hello world","world python hello","world hello","python clojure java world"]
-b = ["hello world python","python world","python"]
+b =["hello world python","python world","python"]
 c=["scala world","python closure world scala"]
 ans=dict((s,[1,s.split(" "),len(s.split(" "))]) for s in a)# creating the ans dict
 root_list=[]
@@ -10,7 +10,6 @@ for key,value in ans.items():
 	if value[2]==min_length:
 		root_list.append(key)
 root_list=list(set(root_list))# remove duplicate strings
-all_lengths.remove(min_length)
 for key,value in ans.items():
 		for r in root_list: 
 			if (set(ans[r][1]).issubset(set(value[1]))):# check for subset
@@ -19,13 +18,3 @@ for key,value in ans.items():
 		if ans[key][0]==1:
 			root_list.append(key)# update root list
 print root_list
-# for l in all_lengths:
-# 	for key,value in ans.items():
-# 		if value[2]==l:
-# 			for r in root_list: 
-# 				if (set(ans[r][1]).issubset(set(value[1]))):# check for subset
-# 					ans[key][0]=0
-# 					break;
-# 			if ans[key][0]==1:
-# 				root_list.append(key)# update root list
-# print root_list
