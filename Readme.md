638fdd72f579cb1833084ee1e01b94f7e022fbff
wizsid11 <siddharth.mahapatra95@gmail.com>
2016-10-19 01:42:54 +0530

    change in solution and readme

--- Readme.md
+++ Readme.md
@@ -1,9 +1,6 @@
 ##Solution To Assignment
-1. The set of all strings of minimum length make up the initial root.
-2. Break the root strings into words and store them in a list(Parent list).
-3. Check if the difference between the set of words in strings of length greater than the minimum length and Parent list is greater than 1.
-4. If the difference is greater than 1 then break it into words and store it in parent list.
-5. We Maintain a dictionary(ans_dict) where we store all the strings as key and their components(words),length of the string and boolean(0 or 1) in the form of a list as values.
-6. Whenever we break a string satisfying point 4 into words and store it in the parent list we also change its boolean value from 0 to 1 in the ans dictinary.
-7. If the difference is equal to 1 we just break a string into its components and store it in the parent list and don't change its boolean to 1.
-8. At last we print all the keys in ans_dict whose boolean is 1.
+1. The set of all unique strings of minimum length make up the initial root list.
+2. We maintain a dictionary of all strings as key and a list containing a boolean(0 or 1),set of words in the string and the length of the string as values.
+3. Then we check if the set of words in any of the root string is a subset of set of words in another string.
+4. If no we add the string to the list of root strings.
+
--- assgn.py
+++ assgn.py
@@ -1,30 +1,22 @@
 import numpy as np
 a=["hello world python","scala world","java python scala hello world","world python hello","world hello","python clojure java world"]
 b =  ["hello world python","python world","python"]
-parent_list=[]
-ans=dict((s,[0,s.split(" "),len(s.split(" "))]) for s in a)# creating the ans dict
+c=["scala world","python closure world scala"]
+root_list=[]
+ans=dict((s,[1,s.split(" "),len(s.split(" "))]) for s in a)# creating the ans dict
 all_lengths = np.array(list(set(map(lambda x:x[2],ans.values())))).tolist()
 min_length = np.array(list(set(map(lambda x:x[2],ans.values())))).min()# calculating the minimum length
 for key,value in ans.items():
 	if value[2]==min_length:
-		ans[key][0]=1
-		parent_list=list(set(parent_list+value[1]))# creating the parent list from string of minimu length 
+		root_list.append(key)
 all_lengths.remove(min_length)
 for l in all_lengths:
 	for key,value in ans.items():
 		if value[2]==l:
-			if(len(set(value[1]))>=len(set(parent_list))):
-				if(len(set(value[1])-set(parent_list)))>1:
-					ans[key][0]=1# changing boolean value
-					parent_list=list(set(parent_list+value[1]))# updating parent list
-				else:
-					parent_list=list(set(parent_list+value[1]))
-			elif(len(set(parent_list))>=len(set(value[1]))):
-				if(len(set(parent_list)-set(value[1])))>1:
-					ans[key][0]=1# changing boolean value
-					parent_list=list(set(parent_list+value[1]))# updating parent list
-				else:
-					parent_list=list(set(parent_list+value[1]))# no change in boolean value only updating parent list
-for key,value in ans.items():
-	if value[0]==1:
-		print key
+			for r in root_list: 
+				if (set(ans[r][1]).issubset(set(value[1]))):
+					ans[key][0]=0
+					break;
+			if ans[key][0]==1:
+				root_list.append(key)
+print root_list
