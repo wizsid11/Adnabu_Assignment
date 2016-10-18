9907b4b989c3f5c710159e4324c4c882f99e1de3
wizsid11 <siddharth.mahapatra95@gmail.com>
2016-10-19 01:50:14 +0530

    change in solution

--- assgn.py
+++ assgn.py
@@ -1,6 +1,6 @@
 import numpy as np
 a=["hello world python","scala world","java python scala hello world","world python hello","world hello","python clojure java world"]
-b =  ["hello world python","python world","python"]
+b = ["hello world python","python world","python"]
 c=["scala world","python closure world scala"]
 root_list=[]
 ans=dict((s,[1,s.split(" "),len(s.split(" "))]) for s in a)# creating the ans dict
@@ -9,14 +9,15 @@ min_length = np.array(list(set(map(lambda x:x[2],ans.values())))).min()# calcula
 for key,value in ans.items():
 	if value[2]==min_length:
 		root_list.append(key)
+root_list=list(set(root_list))# remove duplicate strings
 all_lengths.remove(min_length)
 for l in all_lengths:
 	for key,value in ans.items():
 		if value[2]==l:
 			for r in root_list: 
-				if (set(ans[r][1]).issubset(set(value[1]))):
+				if (set(ans[r][1]).issubset(set(value[1]))):# check for subset
 					ans[key][0]=0
 					break;
 			if ans[key][0]==1:
-				root_list.append(key)
+				root_list.append(key)# update root list
 print root_list
